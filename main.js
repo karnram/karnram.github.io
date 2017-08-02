dcaebae954795825bf4986649f997fd1a158f462
Karn <karnram@186590d6cae7.ant.amazon.com>
2017-08-02 16:10:59 +0530

    Add play button

---
+++ main.js
@@ -0,0 +1,31 @@
+//pause
+$('#pause').click( function() {
+    $('#player').get(0).pause();
+});
+
+//play
+$('#play').click( function() {
+    debugger;
+    $('#player').get(0).play();
+});
+
+//stop
+$('#stop').click( function() {
+    $('#player').get(0).load();
+});
+
+//previous track
+$('#prev').click( function() {
+    //not implemented
+});
+
+//next track
+$('#next').click( function() {
+    //not implemented
+});
+
+//record audio from mic
+$('#record').click( function() {
+    //not yet implemented
+});
+
