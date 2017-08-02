9bcc71064fed5fbf15ff3a118f4ee8af7ddca8f2
Karn <karnram@186590d6cae7.ant.amazon.com>
2017-08-02 16:26:59 +0530

    Fix main.js

--- main.js
+++ main.js
@@ -1,31 +1,4 @@
-//pause
-$('#pause').click( function() {
-    $('#player').get(0).pause();
-});
-
 //play
 $('#play').click( function() {
-    debugger;
     $('#player').get(0).play();
 });
-
-//stop
-$('#stop').click( function() {
-    $('#player').get(0).load();
-});
-
-//previous track
-$('#prev').click( function() {
-    //not implemented
-});
-
-//next track
-$('#next').click( function() {
-    //not implemented
-});
-
-//record audio from mic
-$('#record').click( function() {
-    //not yet implemented
-});
-
